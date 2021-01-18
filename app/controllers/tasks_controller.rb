26ffade3afd05139e9df9640a7edb67f96774cc7
tokiko.aoyama <tokikoaoyama@gmail.com>
2021-01-18 12:49:41 +0000

    login, logout, and register

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:show, :edit, :update, :destroy]
     
     def index
-        @tasks = Task.all.page(params[:page]).per(3)
+        if logged_in?
+            @task = current_user.tasks.build
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        end
     end
     
     def show
@@ -13,12 +17,12 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
-        
+        @task = current_user.tasks.build(task_params)
         if @task.save
             flash[:success] = "Task was successfully posted!"
             redirect_to @task
-        else 
+        else
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
             flash.now[:danger] = "Failed to post the task :("
             render :new
         end
@@ -40,17 +44,20 @@ class TasksController < ApplicationController
     def destroy
         @task.destroy
         
-        flash[:success] = "Task was successfully deleted"
+        flash[:success] = "Task was successfully deleted!"
         redirect_to tasks_url
     end
     
     private
 
-    def set_task
-        @task = Task.find(params[:id])
-    end
-
     def task_params
         params.require(:task).permit(:content, :status)
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
     end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.order(id: :desc).page.params([:page]).per(25)
+  end
+  
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'You have successfully registered!'
+      redirect_to @task
+    else
+      flash[:danger] = 'Failed to register :('
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
